ff47cd15d1aff328c38d5b8e78fe6d41fd167ce7
Gerhard ter Mors <gerhard@termors.com>
2026-01-08 18:51:06 +0100

    2.15

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] //localhost:5001/api/members
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+        {
+            var members = context.Users.ToList();
+            return Ok(members);
+        }
+
+        [HttpGet("{id}")] //localhost:5001/api/members/:id
+        public ActionResult<AppUser> GetMember(string id)
+        {
+            var member = context.Users.Find(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return Ok(member);
+        }
+    }
+}
