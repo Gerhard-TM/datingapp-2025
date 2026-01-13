5bf90e0cfb50aa56d75fc2282a56dbfa72bd3e7d
Gerhard ter Mors <gerhard@termors.com>
2026-01-13 10:55:48 +0100

    4.35

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,23 +1,28 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context) : BaseApiController
 {
     [HttpPost("register")] //api/account/register
-    public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await EmailExists(registerDto.Email))
+            return BadRequest("Email is already taken");
+    
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            Email = email,
-            DisplayName = displayName,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            Email = registerDto.Email,
+            DisplayName = registerDto.DisplayName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -26,5 +31,9 @@ public class AccountController(AppDbContext context) : BaseApiController
 
         return user;
     }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower());
+    }
 }
-        
